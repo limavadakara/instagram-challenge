9d4a81e618ce01c45f77f9560484c47f01898d9e
Lima Vadakara <lima.vadakara@gmail.com>
2019-03-30 19:27:33 +0000

    Add devise to get user functionality and routes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,7 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
@@ -26,6 +27,9 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+
+ # Define url options for development environment
+ config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
